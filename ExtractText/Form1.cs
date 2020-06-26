8ae163a9e9afb676057a8806ec29fb89d6f1287f
carlo <carlojameslim1@gmail.com>
2020-06-26 08:46:17 +0800

    cleanup

--- ExtractText/Form1.cs
+++ ExtractText/Form1.cs
@@ -2,13 +2,7 @@
 using Emgu.CV.OCR;
 using Emgu.CV.Structure;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
